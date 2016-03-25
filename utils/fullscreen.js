93d9e26b6bb478471e7708efde11646f5aa003dc
zhiang.chang <zhiang.chang@qunar.com>
2016-03-25 20:28:32 +0800

    修改样式

--- utils/fullscreen.js
+++ utils/fullscreen.js
@@ -1,21 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-	$('.fullscreen-button').on('click', function(){
+    $('.fullscreen-button').on('click', function () {
 
-		// Set the element you want to go fullscreen
-		var elm = $('.fullscreen-box')[0]
+        // Set the element you want to go fullscreen
+        var elm = $('.fullscreen-box')[0];
 
-		// Now request fullscreen on the element. Need to use browser prefix.
-		if (elm.requestFullscreen) {
-          elm.requestFullscreen();
+        // Now request fullscreen on the element. Need to use browser prefix.
+        if (elm.requestFullscreen) {
+            elm.requestFullscreen();
         } else if (elm.msRequestFullscreen) {
-          elm.msRequestFullscreen();
+            elm.msRequestFullscreen();
         } else if (elm.mozRequestFullScreen) {
-          elm.mozRequestFullScreen();
+            elm.mozRequestFullScreen();
         } else if (elm.webkitRequestFullscreen) {
-          elm.webkitRequestFullscreen();
+            elm.webkitRequestFullscreen();
         }
-	});
+    });
 
 });
 
