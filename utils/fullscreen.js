ea3c61c07fa2f382f78825adf86df4df7633d3f1
zhiang.chang <zhiang.chang@qunar.com>
2016-03-25 20:02:20 +0800

    修改样式

--- utils/fullscreen.js
+++ utils/fullscreen.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 
-	$('.fullscreen-button-box').on('click', function(){
+	$('.fullscreen-button').on('click', function(){
 
 		// Set the element you want to go fullscreen
-		var elm = $(this).parent().parent().parent()[0];
+		var elm = $('.fullscreen-box')[0]
 
 		// Now request fullscreen on the element. Need to use browser prefix.
 		if (elm.requestFullscreen) {
